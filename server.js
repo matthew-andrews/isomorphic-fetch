ffb31344c044d777106bc2813735d0c1acfb5adb
Matthew Andrews <matt@mattandre.ws>
2014-11-07 21:08:06 +0000

    Get isomorphic fetch working

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 require('es6-promise').polyfill();
 global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var noConflictWindow = global.window;
+global.window = {};
 require('fetch');
+global.fetch = global.window.fetch;
+global.window = noConflictWindow;
