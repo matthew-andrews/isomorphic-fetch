afe4752f6f739d48d9ab7750de9d53e42320762e
SamGiles <samuel.giles@ft.com>
2015-01-19 00:22:29 +0000

    Include Headers, Response and Request objects on the global scope on the server

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ var noConflictSelf = global.self;
 global.self = {};
 require('whatwg-fetch');
 global.fetch = global.self.fetch;
+global.Headers = global.self.Headers;
+global.Request = global.self.Request;
+global.Response = global.self.Response;
 global.self = noConflictSelf;
