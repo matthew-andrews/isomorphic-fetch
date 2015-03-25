2c79c44a92315f34efdb701f0e218ef149aa637e
Robert Knight <robert.knight@mendeley.com>
2015-03-25 19:16:19 +0000

    Fix module exports from Webpack / Browserify
    In Webpack / Browserify, require('isomorphic-fetch')
    returned an empty object because the whatwg-fetch
    polyfill only installs fetch() on the global object.
    
    Also correct URLs in package.json
    
    Fixes #16

--- npm-client.js
+++ npm-client.js
@@ -1,1 +1,6 @@
-module.exports = require('whatwg-fetch');
+// the whatwg-fetch polyfill installs the fetch() function
+// on the global object (window or self)
+//
+// Return that as the export for use in Webpack, Browserify etc.
+require('whatwg-fetch');
+module.exports = self.fetch;
