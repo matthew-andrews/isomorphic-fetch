d77255a3e9dfcff3eba2968df73f944da4066888
Nick Baugh <niftylettuce@gmail.com>
2015-11-29 18:25:18 -0500

    Added support for `frisbee` tests

--- fetch-npm-node.js
+++ fetch-npm-node.js
@@ -1,21 +1,22 @@
 "use strict";
 
-var realFetch = (typeof window === 'undefined')
-  ? require('node-fetch')
-  : require('whatwg-fetch');
-
-module.exports = realFetch;
-
-if (typeof window !== 'undefined' && !window.fetch) {
-  window.fetch = module.exports;
-  window.Response = realFetch.Response;
-  window.Headers = realFetch.Headers;
-  window.Request = realFetch.Request;
+// add support for jsdomify for running tests
+// <https://github.com/niftylettuce/frisbee>
+// <https://github.com/podio/jsdomify>
+// <https://github.com/github/fetch/issues/238>
+if (typeof window === 'object') {
+	if (window.fetch)
+		throw new Error('`window.fetch` is already defined');
+	// sets `window.fetch` along with `Response`, `Request`, and `Headers`
+	// see pull github/fetch/issues/238 for this getting added to `window`
+	// <https://github.com/github/fetch/blob/master/fetch.js#L315-L317>
+  require('whatwg-fetch');
+} else if (typeof global === 'object') {
+	if (global.fetch)
+		throw new Error('`global.fetch` is already defined');
+  global.fetch = require('node-fetch');
+  global.Request = global.fetch.Request;
+  global.Response = global.fetch.Response;
+  global.Headers = global.fetch.Headers;
 }
 
-if (typeof global !== 'undefined' && !window.fetch) {
-  global.fetch = module.exports;
-  global.Response = realFetch.Response;
-  global.Headers = realFetch.Headers;
-  global.Request = realFetch.Request;
-}
