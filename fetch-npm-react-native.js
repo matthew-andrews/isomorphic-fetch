676bd2e409dde9749eb60ea906ec66833139ce79
Jiayong Ou <jiayong.ou@ymc.ch>
2016-03-22 10:16:09 +0100

    Remove superfluous newline

--- fetch-npm-react-native.js
+++ fetch-npm-react-native.js
@@ -1,4 +1,3 @@
 // Self is defined in Chrome Debugger, but not in JavaScriptCore
 var globalObject = typeof self === "undefined" ? global : self;
 module.exports = globalObject.fetch.bind(globalObject);
-
