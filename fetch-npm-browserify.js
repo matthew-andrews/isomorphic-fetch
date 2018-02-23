c3518bde9cbafc740f77bd2f863ed612bc72dcf9
Richa Bhardwaj <bhardwaj.richa17@gmail.com>
2018-02-23 12:20:56 +0530

    fixed self issue

--- fetch-npm-browserify.js
+++ fetch-npm-browserify.js
@@ -3,4 +3,5 @@
 //
 // Return that as the export for use in Webpack, Browserify etc.
 require('whatwg-fetch');
-module.exports = self.fetch.bind(self);
+var globalObj = typeof self === 'undefined' ? global : self;
+module.exports = globalObj.fetch.bind(globalObj);
