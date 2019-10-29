12b6085f81f765cf876899e87ccb55a4e5db5cda
Kirill Vorobyov <iridethesun@hotmail.com>
2019-10-29 21:50:46 +0300

    Update fetch-npm-browserify.js

--- fetch-npm-browserify.js
+++ fetch-npm-browserify.js
@@ -3,4 +3,6 @@
 //
 // Return that as the export for use in Webpack, Browserify etc.
 require('whatwg-fetch');
-module.exports = self.fetch.bind(self);
+// module.exports = self.fetch.bind(self);
+var globalObject = typeof self === "undefined" ? global : self;
+module.exports = globalObject.fetch.bind(globalObject);
