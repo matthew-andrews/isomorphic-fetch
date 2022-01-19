463d1996f5144ace83f6624da8655ef9bd1ba78b
Frank Chiarulli Jr <frank@svix.com>
2022-01-19 15:41:55 -0500

    enable keepalives in node

--- fetch-npm-node.js
+++ fetch-npm-node.js
@@ -1,11 +1,29 @@
 "use strict";
 
+var http = require('http');
+var https = require('https');
 var realFetch = require('node-fetch');
+
+const httpAgent = new http.Agent({
+	keepAlive: true
+});
+const httpsAgent = new https.Agent({
+	keepAlive: true
+});
+
+const agent = function(_parsedURL) {
+	if (_parsedURL.protocol == 'http:') {
+		return httpAgent;
+	} else {
+		return httpsAgent;
+	}
+};
+
 module.exports = function(url, options) {
 	if (/^\/\//.test(url)) {
 		url = 'https:' + url;
 	}
-	return realFetch.call(this, url, options);
+	return realFetch.call(this, url, {agent, ...options});
 };
 
 if (!global.fetch) {
