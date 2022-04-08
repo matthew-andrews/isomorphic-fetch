967d7c8a4995d3b85baad8a952d98cbf1bcb558b
ppedziwiatr <peter@redstone.finance>
2022-04-08 13:27:02 +0200

    feat: textencoder/decoder

--- npm-node.js
+++ npm-node.js
@@ -3,6 +3,8 @@
 var undici = require("undici");
 var realFetch = undici.fetch;
 
+const { TextDecoder, TextEncoder } = require(`util`);
+
 module.exports = function (url, options) {
 	if (/^\/\//.test(url)) {
 		url = "https:" + url;
@@ -15,6 +17,12 @@ if (!global.fetch) {
 	global.Response = undici.Response;
 	global.Headers = undici.Headers;
 	global.Request = undici.Request;
+}
+if (!global.TextDecoder) {
+	global.TextDecoder = TextDecoder;
+}
+if (!global.TextEncoder) {
+	global.TextEncoder = TextEncoder;
 }
 
 module.exports.Buffer = require("safe-buffer").Buffer;
